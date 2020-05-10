20327245c1b2b9c7f30ddc38bc5be4ec60808996
A Rohl <april.rohl2015@gmail.com>
2020-05-10 09:57:54 -0700

    installed depencies and updated server.js file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// added dependencies
 var express = require("express");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -17,13 +21,12 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+// adding routes
+require("./routes/api-routes.js")(app);
 
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Syncing to sequelize models and starting the Express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
